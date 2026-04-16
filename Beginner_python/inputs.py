35ba66c441cabdaeab171bb95696428ebec3b2d1
philipfleischer <fleischer1@live.no>
2026-04-16 17:30:54 +0200

    Complete Input learning, Rectangle area calculation, ShoppingCart program

---
+++ Beginner_python/inputs.py
@@ -0,0 +1,10 @@
+# input() = A function that prompts the user to enter data
+    # Returns the entered data as a string
+
+name = input("What is name: ")
+
+print(f"Hello {name}!")
+
+age = int(input("How old are you?: "))
+
+print(f"You are {age + 1} years old, next year!")
---
+++ Beginner_python/rectangle.py
@@ -0,0 +1,5 @@
+# Exercise 1 Rectangle Area Calculation
+length = float(input("Enter length of Rect: "))
+width = float(input("Enter width of Rect: "))
+area = length * width
+print(f"Rectangle area = {area} cm^2")
