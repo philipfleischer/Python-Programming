bd4a076fa59a7fc68528f1e86042d4682422b2df
philipfleischer <fleischer1@live.no>
2026-04-16 17:45:50 +0200

    Arithemtic math operations covered

---
+++ Beginner_python2/arithmetic_operators.py
@@ -0,0 +1,39 @@
+friends = 0
+# Addition --> +
+friends = friends + 1
+print(f"Friends number: {friends}")
+friends += 1
+print(f"Friends number: {friends}")
+# Subtraction --> -
+friends -= 1
+print(f"Friends number: {friends}")
+
+# Multiplication --> *
+friends = 5
+friends = friends * 3
+print(f"Friends number: {friends}")
+friends *= 3
+print(f"Friends number: {friends}")
+
+# Division --> /
+friends = 9
+friends = friends / 3
+print(f"Friends number: {friends}")
+friends = 9
+friends /= 3
+print(f"Friends number: {friends}")
+
+# Power of n --> **
+friends = 2
+print(f"Friends number: {friends}")
+friends = friends ** 2
+print(f"Friends power 2: {friends}")
+friends **= 2
+print(f"Friends number: {friends}")
+
+# Modulus --> %
+remainder = friends % 3
+print(f"remainder of {friends} % 3 = {remainder}")
+friends = 9
+remainder = friends % 3
+print(f"remainder of {friends} % 3 = {remainder}")
