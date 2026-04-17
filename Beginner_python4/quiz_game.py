773b6093f430beedde3fd70a5e0fe14169237350
philipfleischer <fleischer1@live.no>
2026-04-17 12:54:36 +0200

    A really good quiz!

---
+++ Beginner_python4/quiz_game.py
@@ -0,0 +1,60 @@
+# Python quiz game
+
+# need tupple of questions
+questions = ("How many elements are in the periodic table?: ",
+           "Which animal lays the largest eggs?: ",
+           "What is the most abundant gas in Earth's athmosphere?: ",
+           "How many bones are in the human body?: ",
+           "Which planet in the solar system is the hottest?: ")
+
+
+# 2D tuple of options
+options = (("A. 116", "B. 117", "C. 118", "D. 119"),
+           ("A. Whale", "B. Crocodile", "C. Elephant", "D. Ostrich"),
+           ("A. Nitrogen", "B. Oxygen", "C. CO2", "D. H2"),
+           ("A. 206", "B. 207", "C. 208", "D. 209"),
+           ("A. Mercury", "B. Venus", "C. Earth", "D. Mars")
+           )
+
+# A tuple og answers
+answers = ("C", "D", "A", "A", "B")
+
+# A list of guesses, since we want to append
+guesses = []
+
+
+score = 0
+question_num = 0
+
+for question in questions:
+    print("-----------------------------------------------------------")
+    print(question)
+    for option in options[question_num]:
+        print(option)
+
+    guess = input("Enter (A, B, C, D): ").upper()
+    guesses.append(guess)
+    if guess == answers[question_num]:
+        score += 1
+        print("Correct")
+    else:
+        print("Incorrect")
+        print(f"{answers[question_num]} is the correct answer")
+    question_num += 1
+
+print("-----------------------------------------------------------")
+print("             RESULTS                                       ")
+print("-----------------------------------------------------------")
+
+print("answers: ", end="")
+for answer in answers:
+    print(answer, end=" ")
+print()
+
+print("guesses: ", end="")
+for guess in guesses:
+    print(guess, end=" ")
+print()
+
+score = int(score / len(questions) * 100)
+print(f"YOUR SCORE IS: {score}%")
