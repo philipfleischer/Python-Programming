0a6068587961ef695be37e81178acb05c8dcd62d
philipfleischer <fleischer1@live.no>
2026-04-19 14:30:50 +0200

    File detections

---
+++ OOP/file_detection.py
@@ -0,0 +1,9 @@
+# Python file detection
+import os
+
+file_path = "test.txt"
+
+if os.path.exists(file_path):
+    print(f"The location '{file_path}' exists!")
+else:
+    print("That location does not exist!")
