ce0599052735fb44b8a3ab77612d5e9ace36d97d
philipfleischer <fleischer1@live.no>
2026-04-19 14:44:53 +0200

    Read file

---
+++ OOP/file_reading.py
@@ -0,0 +1,12 @@
+# Python reading files
+
+file_path = "output.txt"
+
+try:
+    with open(file_path, "r") as file:
+        content =file.read()
+        print(content)
+except FileNotFoundError:
+    print("That file was not found")
+except PermissionError:
+    print("You do not have permission to read that file")
