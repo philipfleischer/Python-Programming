baa900e13e066c416482923057d0c269210ecd90
philipfleischer <fleischer1@live.no>
2026-04-23 18:44:15 +0200

    Startet OOP DOGS

--- Python_1Hour/oop.py
+++ Python_1Hour/oop.py
@@ -1,1 +1,42 @@
 # Object Oriented Programming in Python
+
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        print(name)
+
+    def get_name(self):
+        return self.name
+
+    def get_age(self):
+        return self.age
+
+    def set_age(self, age):
+        self.age = age
+
+    def add_one(self, x):
+        return x+1
+
+    def bark(self):
+        print("bark")
+
+def main():
+    d = Dog("Svennskoi", 21)
+    d.bark()
+    d.get_age()
+    print(type(d))  # -> <class '__main__.Dog'>
+    print(type(Dog("DS", 22)))  # -> <class '__main__.Dog'>
+    print(d.add_one(6))
+    print(d.get_name())
+
+    d2 = Dog("BOLLI", 83)
+    print(d2.get_name())
+    print(d2.get_age())
+    d2.set_age(90)
+    print(d2.get_age())
+
+
+
+if __name__ == "__main__":
+    main()
